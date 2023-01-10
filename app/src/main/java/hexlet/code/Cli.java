8717a7c501b459ddca75445cd71b0a4144c52f7a
NataliVod <n3824@yandex.ru>
2023-01-10 22:29:48 +0300

    step3

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+ class Cli {
+
+     
+    public static void sayHello() {
+        System.out.println("May I have your name?");
+        Scanner scanner = new Scanner(System.in);
+        String playerName = scanner.next();
+        System.out.println("Hello, " + playerName + "!");
+        scanner.close();
+    }
+}
