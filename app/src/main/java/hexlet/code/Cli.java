95a3b76ed3d6fa1cd19da0e0f685d5e44a163507
NataliVod <n3824@yandex.ru>
2023-01-15 03:59:32 +0300

    fix Greet

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
     public static void sayHello() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
         Scanner scanner = new Scanner(System.in);
         String playerName = scanner.nextLine();
